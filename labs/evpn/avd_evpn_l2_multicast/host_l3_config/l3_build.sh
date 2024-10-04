3232213310b2b6e71438d0378962056481002c27
root <root@localhost.localdomain>
2024-10-04 14:49:02 +0530

    Added iperf/scapy-cli to alpine_host Dockerfile and changed VLAN IDs in l3_build.sh

--- labs/evpn/avd_evpn_l2_multicast/host_l3_config/l3_build.sh
+++ labs/evpn/avd_evpn_l2_multicast/host_l3_config/l3_build.sh
@@ -2,37 +2,37 @@
 
 
 CMD1='cat /etc/hostname; \
-sudo vconfig add team0 110; \
-sudo ifconfig team0.110 10.1.10.101 netmask 255.255.255.0;\
-sudo ip link set up team0.110; \
-sudo ip route add 10.1.0.0/16 via 10.1.10.1 dev team0.110; \
-sudo ifconfig team0.110; \
+sudo vconfig add team0 100; \
+sudo ifconfig team0.100 10.1.10.101 netmask 255.255.255.0;\
+sudo ip link set up team0.100; \
+sudo ip route add 10.1.0.0/16 via 10.1.10.1 dev team0.100; \
+sudo ifconfig team0.100; \
 sudo route -n
 '
 
 CMD2='cat /etc/hostname; \
-sudo vconfig add team0 111; \
-sudo ifconfig team0.111 10.1.11.102 netmask 255.255.255.0; \
-sudo ip link set up team0.111; \
-sudo ip route add 10.1.0.0/16 via 10.1.11.1 dev team0.111; \
-sudo ifconfig team0.111; \
+sudo vconfig add team0 200; \
+sudo ifconfig team0.200 10.1.11.102 netmask 255.255.255.0; \
+sudo ip link set up team0.200; \
+sudo ip route add 10.1.0.0/16 via 10.1.11.1 dev team0.200; \
+sudo ifconfig team0.200; \
 sudo route -n'
 
 CMD3='cat /etc/hostname; \
-sudo vconfig add team0 110; \
-sudo ifconfig team0.110 10.1.10.103 netmask 255.255.255.0; \
-sudo ip link set up team0.110; \
-sudo ip route add 10.1.0.0/16 via 10.1.10.1 dev team0.110; \
-sudo ifconfig team0.110; \
+sudo vconfig add team0 100; \
+sudo ifconfig team0.100 10.1.10.103 netmask 255.255.255.0; \
+sudo ip link set up team0.100; \
+sudo ip route add 10.1.0.0/16 via 10.1.10.1 dev team0.100; \
+sudo ifconfig team0.100; \
 sudo route -n
 '
 
 CMD4='cat /etc/hostname; \
-sudo vconfig add team0 111; \
-sudo ifconfig team0.111 10.1.11.104 netmask 255.255.255.0; \
-sudo ip link set up team0.111; \
-sudo ip route add 10.1.0.0/16 via 10.1.11.1 dev team0.111; \
-sudo ifconfig team0.111; \
+sudo vconfig add team0 200; \
+sudo ifconfig team0.200 10.1.11.104 netmask 255.255.255.0; \
+sudo ip link set up team0.200; \
+sudo ip route add 10.1.0.0/16 via 10.1.11.1 dev team0.200; \
+sudo ifconfig team0.200; \
 sudo route -n'
 
 echo "[INFO] Configuring clab-avdasymirb-client1"
